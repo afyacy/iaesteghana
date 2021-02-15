47055c4807f10b1ca0b59a4ac150df3a3ad693f4
Afyacy <afyacy@gmail.com>
2021-02-15 06:07:04 +0000

    email skeleton

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -2,14 +2,44 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Contact extends CI_Controller {
-	public function index()
-	{
-		$data = [
-			'meta_title' => 'Official IAESTE Ghana Website | About',
-		]; 
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('contact', $data);
-        $this->load->view('templates/footer', $data);
-	}
+	function __construct() {
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->helper('form');
+    }
+    public function index() {
+        $this->load->helper('form');
+        $this->load->view('contact');
+    }
+
+    public function send_mail() {
+        $from_email = $this->input->post('email');
+        $to_email = 'darkoafya@gmail.com';
+        //Load email library
+        $this->load->library('email');
+        $this->email->from($from_email, 'Identification');
+        $this->email->to($to_email);
+        $this->email->subject('Send Email Codeigniter');
+        $this->email->message('The email send using codeigniter library');
+        //Send mail
+        if($this->email->send())
+            $this->session->set_flashdata("email_sent","Congragulation Email Send Successfully.");
+        else
+            $this->session->set_flashdata("email_sent","You have encountered an error");
+        $this->load->view('contact');
+    }
+	 
+
+	// public function index()
+	// {
+	// 	$this->load->library('email');
+	// 	$data = [
+	// 		'meta_title' => 'Official IAESTE Ghana Website | About',
+	// 	]; 
+
+    //     $this->load->view('templates/header', $data);
+    //     $this->load->view('contact', $data);
+    //     $this->load->view('templates/footer', $data);
+	// }
 }
--- application/views/contact.php
+++ application/views/contact.php
@@ -1,4 +1,14 @@
-<div class="contact4 overflow-hiddedn">
+<?php
+    echo $this->session->flashdata('email_sent');
+    echo form_open('/Contact/send_mail');
+    ?>
+    <input type = "email" name = "email" required />
+    <input type = "submit" value = "SEND MAIL">
+    <?php
+    echo form_close();
+?>
+
+<!-- <div class="contact4 overflow-hiddedn">
   <div class="container">
       <div class="row justify-content-md-center">
           <div class="col-sm">
@@ -33,4 +43,4 @@
           </div>
       </div>
       </div>
-</div>
+</div> -->
